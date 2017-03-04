829ba2df2286171346a3d6d435d4b1101f396a19
Pete Kinnecom <git@k7u7.com>
2017-03-04 12:59:36 -0800

    Change configuration structure

--- lib/docker_compose_deploy.rb
+++ lib/docker_compose_deploy.rb
@@ -1,20 +1,37 @@
 require "docker_compose_deploy/version"
 
 module DockerComposeDeploy
-  def self.configure
-    yield(config)
-  end
+  class << self
+    def configure
+      yield(config_by_environment)
+    end
 
-  def self.config
-    @config ||= Configuration.new
-  end
+    def config
+      @config ||= Configuration.new(@environment, config_by_environment[@environment])
+    end
 
-  def self.configure!
-    load('./config.rb')
+    def configure!(environment)
+      load('./config.rb')
+      @environment = environment
+    end
+
+    private
+
+    def config_by_environment
+      @config_by_environment ||= {}
+    end
   end
 
   class Configuration
-    attr_accessor :connection
-    attr_accessor :ignore_pull_failures
+    attr_reader :connection, :ignore_pull_failures
+    def initialize(environment, connection:, ignore_pull_failures: false)
+      @environment = environment
+      @connection = connection
+      @ignore_pull_failures = ignore_pull_failures
+    end
+
+    def sites_path
+      File.join(".", environment, "sites")
+    end
   end
 end
